af669dda45debeb1d21c7416b902eddb56909285
mosses1207 <mosses161192@gmail.com>
2026-05-02 19:43:01 +0700

    Update global.js

--- global.js
+++ global.js
@@ -1,1 +1,9 @@
-
+let currentPos = { lat: 0, lng: 0 };
+let watchId = null;
+let map;
+let userMarker;
+let isFirstLocation = true;
+let isAutoCenter = true;
+let lastAddressLat = 0;
+let lastAddressLng = 0;
+let msg = "";
