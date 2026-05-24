4d3f21b8ddcb0325deeb52a055fcab000b989adb
mosses1207 <mosses161192@gmail.com>
2026-05-24 23:35:22 +0700

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-import Dexie from 'dexie';
-export const db = new Dexie('logistic_db');
-
-db.version(3).stores({
-    all_logs: 'idseason, created_at, saved_at',
-    travel_sessions: 'idseason, status, waktu_berangkat',
-    real_location: 'idseason, created_at, saved_at, sjkb, updated_at, lat, lng',
-});
